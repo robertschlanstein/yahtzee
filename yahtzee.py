0260a9ef671d9a2f50623174ef93ca655e3514f1
Robert Schlanstein <r_s_w@gmx.de>
2025-12-01 12:57:37 +0100

    keep_dice() only allowing keeping actually rolled dice

--- yahtzee.py
+++ yahtzee.py
@@ -1,14 +1,17 @@
 class Dice_cup:
     def __init__(self):
-        self.roll = [0, 0, 0, 0, 0]
+        self.roll = [3, 3, 3, 1, 5]
         self.keep = []
 
     def roll_dice(self, num):
         self.roll[1] = 1
 
-    def keep_dice(self, keep_list):
-        self.keep = sorted(keep_list[:len(self.roll)])
+    def keep_dice(self, keep_input):
+        for d in self.roll:
+            if d in keep_input:
+                self.keep.append(d)
+                keep_input.remove(d)
 
 dc = Dice_cup()
-dc.keep_dice([])
+dc.keep_dice([3, 3])
 print(dc.keep)
