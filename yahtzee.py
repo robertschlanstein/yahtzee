dd3b8a0f39fb4a643d127172124e59d17fc69fcf
Robert Schlanstein <r_s_w@gmx.de>
2025-12-01 15:32:53 +0100

    Add class Score_card and its most basic functions

--- yahtzee.py
+++ yahtzee.py
@@ -19,3 +19,26 @@ class Dice_cup:
             if d in keep_input:
                 self.keep.append(d)
                 keep_input.remove(d)
+
+class Score_card:
+    def __init__(self, num_of_games = 6):
+        self.num_of_games = num_of_games
+        self.match_scores = []
+
+    def new_game(self):
+        self.match_scores.append([0 for i in range(13)])
+
+    def add_score(self, comb_num, score):
+        if comb_num < 1 or comb_num > 13:
+            print("Invalid combination number...")
+            return
+        if score < 0 or score > 50:
+            print("Impossible score...")
+            return
+        if self.match_scores[-1][comb_num - 1] != 0:
+            print("Combination already filled in...")
+            return
+        self.match_scores[-1][comb_num - 1] = score
+        print(self.match_scores)
+
+sc = Score_card()
