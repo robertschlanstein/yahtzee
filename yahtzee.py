e4659437ead8ba00e7608cbc5e2d425272a8dfb1
Robert Schlanstein <r_s_w@gmx.de>
2025-12-01 12:16:45 +0100

    keep_dice() without check for actually rolled dice

--- yahtzee.py
+++ yahtzee.py
@@ -1,7 +1,14 @@
 class Dice_cup:
-    def __init__():
-        roll = [0, 0, 0, 0, 0]
-        keep = []
+    def __init__(self):
+        self.roll = [0, 0, 0, 0, 0]
+        self.keep = []
 
-    def roll_dice(num):
-        pass
+    def roll_dice(self, num):
+        self.roll[1] = 1
+
+    def keep_dice(self, keep_list):
+        self.keep = sorted(keep_list[:len(self.roll)])
+
+dc = Dice_cup()
+dc.keep_dice([])
+print(dc.keep)
