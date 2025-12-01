a730ebd606e1da8c191ad2dda7422e92d99dcb21
Robert Schlanstein <r_s_w@gmx.de>
2025-12-01 13:26:00 +0100

    Define function roll_dice()

--- yahtzee.py
+++ yahtzee.py
@@ -1,14 +1,26 @@
+import random
+
 class Dice_cup:
     def __init__(self, num_of_dice = 5):
         self.num_of_dice = num_of_dice
         self.roll = []
         self.keep = []
 
-    def roll_dice(self, num):
-        self.roll[1] = 1
+    def roll_dice(self):
+        self.roll.clear()
+        print(self.roll)
+        for d in self.keep:
+            self.roll.append(d)
+        print(self.roll)
+        while len(self.roll) < self.num_of_dice:
+            self.roll.append(random.randint(1, 6))
+        print(self.roll)
 
     def keep_dice(self, keep_input):
         for d in self.roll:
             if d in keep_input:
                 self.keep.append(d)
                 keep_input.remove(d)
+
+dc = Dice_cup()
+dc.roll_dice()
